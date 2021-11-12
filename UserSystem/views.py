aaefd21053093b84be7c09fe675a7698b358063e
GodKnows <Chernjakov2@gmail.com>
2021-11-12 22:41:26 +0300

    Forms for auth
    Add form to login and button to logout.

--- UserSystem/views.py
+++ UserSystem/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, UserManager
 from UserSystem.models import UserInfo, UserTypes
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
-def registration (request):
+def registration(request):
     if request.user.is_authenticated:
         return redirect('/')
     if request.method == 'POST':
@@ -21,9 +22,25 @@ def registration (request):
         new_user_info = UserInfo.objects.create(user=new_user, user_type=user_type)
         new_user.save()
         new_user_info.save()
-        return redirect('/')
+        return redirect('/auth/login/')
     return render(request, 'registration.html')
 
 
-def login(request):
-    return render(request, 'login.html')
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'login.html', context={'is_error': True})
+    return render(request, 'login.html', context={'is_error': False})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/auth/login/')
