d1b2cbb031a266fcb8c4b14745f9732121201bd6
Mobhunter <54095546+Mobhunter@users.noreply.github.com>
2021-11-14 00:27:04 +0300

    Rewrite on conversation handler

--- telegram_bot/management/commands/bot.py
+++ telegram_bot/management/commands/bot.py
@@ -1,21 +1,20 @@
 import logging
-from typing import List
 from django.core.management.base import BaseCommand, CommandError
+from telegram.ext.utils.types import ConversationDict
 from telegram.message import Message
 from ... import models as bot_models
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import AnonymousUser, User
 from AgroHackMarketplace.settings import BOT_TOKEN
 from UserSystem import models as usersys_models
-from pathlib import Path
-from AgroHackMarketplace.settings import MEDIA_ROOT
 from telegram import Update, ParseMode, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters, ConversationHandler
 
 # Enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
+LOGIN, PASSWORD, COMMANDS = range(3)
 
 logger = logging.getLogger(__name__)
 
@@ -45,26 +44,26 @@ def get_user_from_update(update: Update) -> User:
         return user
 
 
-def start(update: Update, context: CallbackContext) -> None:
+def start(update: Update, context: CallbackContext) -> int:
     """Sends explanation on how to use the bot."""
 
     update.message.reply_text('Здравствуйте! Проверяем, привязали ли вы учетную запись к боту.\n...')
     info = bot_models.TelegramInfo.objects.filter(chat_id=update.message.from_user.id).first()
     if info is None:
         message = update.message.reply_text('''Похоже, вы не привязали свой телеграмм-аккаунт к учётной записи на нашем сайте.\nВведите логин:''')
-        context.chat_data['entering_login'] = 1
         add_messages_on_clear(context, update.message, message)
-    else:
-        keyboard = None
-        update.message.reply_text(f'Добро пожаловать, {info.user.first_name}')
-        if info.user.user_info.user_type in (usersys_models.UserTypes.RETAIL_PURCHASER, usersys_models.UserTypes.WHOLESALE_PURCHASER):
-            keyboard = [["/requests"]]
-        if keyboard is not None:
-            keyboard = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
-            update.message.reply_text("/requests - список выставленных вами запросов на товары", reply_markup=keyboard)
+        return LOGIN
+    keyboard = None
+    update.message.reply_text(f'Добро пожаловать, {info.user.first_name}')
+    if info.user.user_info.user_type in (usersys_models.UserTypes.RETAIL_PURCHASER, usersys_models.UserTypes.WHOLESALE_PURCHASER):
+        keyboard = [["/requests"]]
+    if keyboard is not None:
+        keyboard = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+        update.message.reply_text("/requests - список выставленных вами запросов на товары", reply_markup=keyboard)
+    return COMMANDS
+        
 
-
-def requests(update: Update, context: CallbackContext) -> None:
+def requests(update: Update, context: CallbackContext) -> int:
     user = get_user_from_update(update)
     if isinstance(user, AnonymousUser):
         update.message.reply_text("Попался, анонимус! ТЫ НЕ ПРОЙДЕШЬ НЕ ЗАРЕГИСТРИРОВАВШИСЬ!")
@@ -94,44 +93,53 @@ def requests(update: Update, context: CallbackContext) -> None:
         keyboard = [["/requests"]]
         keyboard = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
         update.message.reply_text("/requests - список выставленных вами запросов на товары", reply_markup=keyboard)
+    return COMMANDS
 
 
+def on_login(update: Update, context: CallbackContext) -> int:
+    context.chat_data['login'] = update.message.text
+    message = update.message.reply_text('Введите пароль:')
+    add_messages_on_clear(context, update.message, message)
+    return PASSWORD
 
 
-def on_message(update: Update, context: CallbackContext) -> None:
-    if context.chat_data.get('entering_login', 0):
-        del context.chat_data['entering_login']
-        context.chat_data['login'] = update.message.text
-        message = update.message.reply_text('Введите пароль:')
-        context.chat_data['entering_password'] = 1
-        add_messages_on_clear(context, update.message, message)
-
-    elif context.chat_data.get('entering_password', 0):
-        add_messages_on_clear(context, update.message)
-        del context.chat_data['entering_password']
-        login = context.chat_data['login']
-        del context.chat_data['login']
-        password = update.message.text
-        user = authenticate(username=login, password=password)
-        clear_messages(context)
-        if user is not None:
-            add_chatid(user, update.message.chat_id)
-            update.message.reply_text(f'Добро пожаловать, {user.first_name}')
-            keyboard = [["/requests"]]
-            keyboard = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
-            update.message.reply_text("/requests - список выставленных вами запросов на товары", reply_markup=keyboard)
-        else:
-            message = update.message.reply_text('Неправильный логин или пароль\nВведите логин:')
-            add_messages_on_clear(context, message)
-            context.chat_data['entering_login'] = 1
+def on_password(update: Update, context: CallbackContext) -> int:
+    add_messages_on_clear(context, update.message)
+    login = context.chat_data['login']
+    del context.chat_data['login']
+    password = update.message.text
+    user = authenticate(username=login, password=password)
+    clear_messages(context)
+    if user is not None:
+        add_chatid(user, update.message.chat_id)
+        update.message.reply_text(f'Добро пожаловать, {user.first_name}')
+        keyboard = [["/requests"]]
+        keyboard = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+        update.message.reply_text("/requests - список выставленных вами запросов на товары", reply_markup=keyboard)
+        return COMMANDS
+    message = update.message.reply_text('Неправильный логин или пароль\nВведите логин:')
+    add_messages_on_clear(context, message)
+    return LOGIN
 
 
 updater = Updater(BOT_TOKEN)
 dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(CommandHandler("requests", requests))
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, callback=on_message))
-
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler("start", start)],
+    states={
+        LOGIN: [
+            MessageHandler(Filters.text & ~Filters.command, on_login),
+        ],
+        PASSWORD: [
+            MessageHandler(Filters.text & ~Filters.command, on_password)
+        ],
+        COMMANDS: [
+            CommandHandler("requests", requests)
+        ],
+    },
+    fallbacks=[CommandHandler("start", start)]
+)
+dispatcher.add_handler(conv_handler)
 
 def send_notification(user: User, text: str):
     bot = updater.bot
