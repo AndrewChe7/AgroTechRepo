90f9e560ddf222190fcb13606cbf8f312053d929
Mobhunter <54095546+Mobhunter@users.noreply.github.com>
2021-11-13 07:43:21 +0300

    Very small pullrequest fix

--- AgroHackMarketplace/settings.py
+++ AgroHackMarketplace/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.environ['STATIC_ROOT']
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
